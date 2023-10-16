97e802a772d5b1f6738f680ea8d734e4bd7f2163
Benjamin Wagner <51082373+btinet@users.noreply.github.com>
2023-10-16 07:36:08 +0200

    initial commit

---
+++ assets/app.js
@@ -0,0 +1,21 @@
+import { registerReactControllerComponents } from '@symfony/ux-react';
+import './bootstrap.js';
+
+import DataTable from 'datatables.net-dt';
+import 'datatables.net-responsive-dt';
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.scss';
+
+
+let table = new DataTable('#example', {
+    responsive: true
+});
+
+registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
